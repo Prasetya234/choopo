cf4ab4a7422c6f293f2a1e6beccf61cf4662e07c
fighantalenta_ <tatamf.id@gmail.com>
2021-04-28 12:53:25 +0700

    Update

--- src/main/java/com/example/choopo/repository/UserTypeRepository.java
+++ src/main/java/com/example/choopo/repository/UserTypeRepository.java
@@ -4,4 +4,5 @@ import com.example.choopo.model.UserType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserTypeRepository extends JpaRepository<UserType, Long> {
+
 }
