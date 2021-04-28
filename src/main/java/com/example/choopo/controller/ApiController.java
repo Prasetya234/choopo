6b498e8eb11b22019c8c0d5e1973cf6e2ef0c4a8
fighantalenta_ <tatamf.id@gmail.com>
2021-04-28 11:49:05 +0700

    Update

--- src/main/java/com/example/choopo/controller/ApiController.java
+++
@@ -1,9 +0,0 @@
-package com.example.choopo.controller;
-
-public class ApiController {
-
-//    @GetMapping
-//    public ResponseEntity<?> kata() {
-//        return ResponseEntity.ok("Hello World");
-//    }
-}
