61abbb1e27ca00a4e5551a25094021670293b721
Prasetya234 <putraprasetyadhany@gmail.com>
2021-05-03 11:04:53 +0700

    m

--- src/main/java/com/example/choopo/ChoopoApplication.java
+++ src/main/java/com/example/choopo/ChoopoApplication.java
@@ -2,8 +2,6 @@ package com.example.choopo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class ChoopoApplication {
